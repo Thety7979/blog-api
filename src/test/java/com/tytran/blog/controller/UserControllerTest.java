c96b024b9aaa2660f7f1382048cc52b6c2ef9748
Thety7979 <thety22112005@gmail.com>
2025-11-11 20:22:04 +0700

    feat: add H2 database dependency for testing; configure test properties for in-memory database

--- src/test/java/com/tytran/blog/controller/UserControllerTest.java
+++ src/test/java/com/tytran/blog/controller/UserControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest
 @Slf4j
 @AutoConfigureMockMvc
+@TestPropertySource("classpath:test.properties")
 public class UserControllerTest {
 
     @Autowired
--- src/test/java/com/tytran/blog/service/UserServiceTest.java
+++ src/test/java/com/tytran/blog/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +25,7 @@ import com.tytran.blog.services.UserService;
 
 @SpringBootTest
 @Transactional
+@TestPropertySource("classpath:test.properties")
 public class UserServiceTest {
 
     @Autowired
